fb402b7fc82e70fe2dd140092ea94cbaeea0a40e
FARHAD  NAJAFOV <farhadnajafov98.com>
2022-11-10 13:46:37 +0400

    Joke Button

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 
-let div=document.querySelector("div")
-const mas=[5,3,1,2,0,-2]
+let div=document.querySelectorAll("div")
+let btn=document.createElement('BUTTON')
+btn.innerHTML+="button"
+div[0].append(btn)
 
-mas.forEach((item,index)=>{
-div.innerText+=" "+item;
-})
+function changebtnName(event){
+    event.target.innerText="Click"
+}
+btn.addEventListener("click",changebtnName)
+
+
